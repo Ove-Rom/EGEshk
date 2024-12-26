0009908b4e3c6ebbc81cc77ad11e1236341d9b45
Роман <rmanuylov.r@gmail.com>
2024-12-26 21:25:24 +0200

    2377

---
+++ EGEkr-21-12-24/task 24 - 19254.py
@@ -0,0 +1,4 @@
+with open("24_19254.txt") as file:
+    data = file.read().replace("FSRQ", "FSRQ ").split()
+
+print(max(((sum(len(data[j]) for j in range(i, i + 81)) for i in range(len(data) - 80)))))
