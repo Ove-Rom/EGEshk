bfb1e8c18620d28eade14b07ea70489fc681e56c
Роман <rmanuylov.r@gmail.com>
2024-12-26 23:09:27 +0200

    решить не смог, был повержен генератором

--- EGEkr-21-12-24/task 26 - 19256.py
+++ EGEkr-21-12-24/task 26 - 19256.py
@@ -1,11 +1,11 @@
-with open("26_19256.txt") as file:
-    file.readline()
-    data = file.read().split("\n")
-
+# with open("26_19256.txt") as file:
+#     file.readline()
+#     data = file.read().split("\n")
+data = ((60, 33), (60, 33), (50, 125), (50, 126), (50, 127), (50, 72), (50, 126))
 base = dict()
 
 for info in data:
-    info = info.split()
+    # info = info.split()
     id = int(info[0])
     num = int(info[1])
     if id not in base:
@@ -13,13 +13,16 @@ for info in data:
     else:
         base[id].append(num)
 
+nums = [i for i in range(1, 100001)]
 leaderId = 0
 leaderNums = 0
 
 for student in base.items():
     student = list(student)
-    student[1] = sorted(student[1])
-    if all(student[1][j] + 1 == student[1][j+1] for j in range(len(student[1]) - 1)):
+    student[1] = list(set(sorted(student[1])))
+    if any(student[1][j] + 1 == student[1][j+1] for j in range(len(student[1]) - 1)):
+        count = max(j - i + 1 for i in range(len(student[1])) for j in range(i+1, len(student[1])-1) if student[1][i:j+1] in nums)
+
         if leaderNums < len(student[1]):
             leaderId = student[0]
             leaderNums = len(student[1])
