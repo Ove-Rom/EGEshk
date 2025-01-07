43e52cd3eef8cf786ad287c7221724aaf77251d5
Роман <rmanuylov.r@gmail.com>
2025-01-07 16:34:50 +0200

    627

--- 7-1-25/task 14 - 12246.py
+++ 7-1-25/task 14 - 12246.py
@@ -7,6 +7,14 @@ def toq(a, q):
 
 num = 2 * 729 ** 333 + 2 * 243 ** 334 - 81 ** 335 + 2 * 27 ** 336 - 2 * 9 ** 337 - 338
 
+# count = 0
+# while num:
+#     if num % 9:
+#         count += 1
+#     num //= 9
+#
+# print(count)
+
 num = toq(num, 9)
 
 print(sum(i in "12345678" for i in num))
