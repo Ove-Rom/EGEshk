a1803e2bdb3eea2e1cbc6da0e29d02786b0d65e3
Роман <rmanuylov.r@gmail.com>
2025-01-21 16:58:52 +0200

    9

--- 21-1-25/task 15 - 4995.py
+++ 21-1-25/task 15 - 4995.py
@@ -3,8 +3,8 @@ def f(a):
     for x in range(1, 1000):
             for y in range(1, 1000):
                 f1 = (x + y <= 22) or (y <= x - 6) or (y >= a)
-                ans.append(f1)
-    return all(ans)
+                if not f1: return 0
+    else: return 1
 
 for a in range(1000, 0, -1):
     if f(a):
