8007b8897c6a89bbf0733b07572fb88df3ad2041
Роман <rmanuylov.r@gmail.com>
2025-01-24 23:05:26 +0200

    85 195

---
+++ DZshkas/OMEGAprobnik/task 17/task 17.py
@@ -0,0 +1,16 @@
+with open("17_8562.txt") as file:
+    data = [int(i) for i in file]
+
+minn = min(i for i in data if len(str(abs(i))) == 2 and str(i)[-1] == '1') ** 2
+
+count = 0
+minS = 99999999999
+
+for i in range(len(data) - 1):
+    c1 = data[i] ** 2 < minn <= data[i + 1] ** 2
+    c2 = data[i] + data[i+1] >= 0
+    if c1 & c2:
+        count += 1
+        minS = min(minS, data[i] + data[i+1])
+
+print(count, minS)
