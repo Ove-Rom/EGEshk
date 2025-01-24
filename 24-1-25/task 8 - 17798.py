927ca70406b5555dac70c8290298c7548c4a06d6
Роман <rmanuylov.r@gmail.com>
2025-01-24 14:16:31 +0200

    513

---
+++ 24-1-25/task 8 - 17798.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+s = "мнс"
+g = "иу"
+c = 0
+
+for i in product(sorted("минус"), repeat = 4):
+    c1 = sum(i.count(j) for j in s)
+    c2 = sum(i.count(j) for j in g)
+    if c1 >= c2:
+        c += 1
+print(c)
