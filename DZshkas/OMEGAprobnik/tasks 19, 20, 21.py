20b5eb9b5a57be6ee89745d6ae4ce26101df5347
Роман <rmanuylov.r@gmail.com>
2025-01-25 16:13:26 +0200

    19) 139 20) 129 138 21) 63

--- DZshkas/OMEGAprobnik/tasks 19, 20, 21.py
+++ DZshkas/OMEGAprobnik/tasks 19, 20, 21.py
@@ -1,10 +1,10 @@
 def f(a, b, m):
     if (a + b) >= 300: return m % 2 == 0
     if m == 0: return 0
-    h = [f(a + 2, b, m - 1), f(a, b + 2, m - 1),
+    h = [f(a + 3, b, m - 1), f(a, b + 3, m - 1),
          f(a * 2, b, m - 1), f(a, b * 2, m - 1)]
-    return all(h) if m % 2 == 0 else any(h)
+    return any(h) if (m + 1) % 2 == 0 else all(h)
 
-print("20)", *[a for a in range(1, 280) if f(a, 20, 2)])
+print("19)", *[a for a in range(1, 280) if f(a, 20, 2)])
 print("20)", *[a for a in range(1, 280) if f(a, 20, 3) and not f(a, 20, 1) ])
-print("20)", *[a for a in range(1, 280) if f(a, 20, 5) and not f(a, 20, 3) ])
+print("21)", *[a for a in range(1, 280) if f(a, 20, 5) and not f(a, 20, 3) ])
