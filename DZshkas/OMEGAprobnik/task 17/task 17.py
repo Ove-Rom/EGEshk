fcba2d826ba608be7f046c312076ce145b06327a
Роман <rmanuylov.r@gmail.com>
2025-01-25 18:54:20 +0200

    157 113

--- DZshkas/OMEGAprobnik/task 17/task 17.py
+++ DZshkas/OMEGAprobnik/task 17/task 17.py
@@ -8,8 +8,9 @@ minS = 99999999999
 
 for i in range(len(data) - 1):
     c1 = data[i] ** 2 < minn <= data[i + 1] ** 2
-    c2 = data[i] + data[i+1] >= 0
-    if c1 & c2:
+    c2 = data[i+1] ** 2 < minn <= data[i] ** 2
+    c3 = data[i] + data[i+1] >= 0
+    if (c1 | c2) & c3:
         count += 1
         minS = min(minS, data[i] + data[i+1])
 
