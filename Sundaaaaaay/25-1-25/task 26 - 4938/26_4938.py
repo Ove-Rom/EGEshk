896a5548892d10b8fc71ff47cc72d4e2565171c4
Роман <rmanuylov.r@gmail.com>
2025-01-25 17:36:59 +0200

    91 9881

---
+++ Sundaaaaaay/25-1-25/task 26 - 4938/26_4938.py
@@ -0,0 +1,13 @@
+with open("26_4938.txt") as f:
+    maxTime = int(f.readline().split()[0])
+    data = [[int(i.split()[0]), int(i.split()[1])] for i in f]
+
+data = sorted(data, key=lambda x: x[1])
+
+start = [data[0]]
+
+for i in data:
+    if start[-1][1] <= i[0]:
+        start.append(i)
+
+print(len(start), start[-1][0])
