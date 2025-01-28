bacf870f267c68ca217f93908ce73b29a765cf79
Роман <rmanuylov.r@gmail.com>
2025-01-28 14:45:45 +0200

    494

---
+++ 28-1-25/task 5 - 14331.py
@@ -0,0 +1,19 @@
+def toq(a, q):
+    ans = ''
+    while a:
+        ans += str(a % q)
+        a //= q
+    return ans[::-1]
+
+ans = []
+for n in range(1000):
+    r = toq(n, 3)
+    s = sum(int(i) for i in r)
+    if s % 3 == 0:
+        r += "212"
+    else:
+        r += toq((s % 3) * 2, 3)
+    r = int(r, 3)
+    if r > 490: ans.append(r)
+
+print(min(ans))
