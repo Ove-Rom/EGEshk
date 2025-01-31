83865af8859ecb9c69bfacde75d0a9b1687e42a5
Роман <rmanuylov.r@gmail.com>
2025-01-31 16:45:37 +0200

    2624400

--- Normeses/31-1-25/task 8 - 18049.py
+++ Normeses/31-1-25/task 8 - 18049.py
@@ -9,9 +9,9 @@ def toq(a, q):
 count = 0
 
 for i in range(int("1000000", 9), int("10000000", 9)):
-    j = str(i)
+    j = toq(i, 9)
     c1 = j[0] not in "246"
-    c2 = j[-3:] != j[0] * 3
+    c2 = j[-3:] != j[-1] * 3
     if c1 & c2: count += 1
 
 print(count)
