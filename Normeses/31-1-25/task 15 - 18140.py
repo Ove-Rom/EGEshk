d51b6a91a22e5be4a32a7720984e72efdc021f52
Роман <rmanuylov.r@gmail.com>
2025-01-31 16:52:03 +0200

    22

--- Normeses/31-1-25/task 15 - 18140.py
+++ Normeses/31-1-25/task 15 - 18140.py
@@ -1,4 +1,4 @@
 for a in range(1000, 0, -1):
-    if all((x-y>=39) or (y<=x) or (y>=a-20) for x in range(1000) for y in range(1000)):
+    if all((x-y>=39) or (y<=x) or (y>=a-20) for x in range(1, 1000) for y in range(1, 1000)):
         print(a)
         break
