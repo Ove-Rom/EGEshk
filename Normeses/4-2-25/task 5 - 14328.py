37be30f294f403f8cac40410a0554621399b1c44
Роман <rmanuylov.r@gmail.com>
2025-02-04 16:20:38 +0200

    1991

---
+++ Normeses/4-2-25/task 5 - 14328.py
@@ -0,0 +1,24 @@
+from string import digits, ascii_lowercase
+
+alph = digits + ascii_lowercase
+
+
+def toq(a, q):
+    ans = ''
+    while a:
+        ans += alph[a % q]
+        a //= q
+    return ans[::-1]
+
+
+ans = []
+for n in range(10000):
+    r = toq(n, 12)
+    if n % 3 == 0:
+        r = "1" + r + "b"
+    else:
+        r = "2" + r + "0"
+    r = int(r, 12)
+    if r < 1996: ans.append(r)
+
+print(max(ans))
