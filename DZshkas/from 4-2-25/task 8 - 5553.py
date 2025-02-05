c4a5edb32bed3432ffd403bb442a944aad88f568
Роман <rmanuylov.r@gmail.com>
2025-02-05 19:51:45 +0200

    1800

--- DZshkas/from 4-2-25/task 8 - 5553.py
+++ DZshkas/from 4-2-25/task 8 - 5553.py
@@ -2,10 +2,10 @@ from itertools import permutations
 
 count = 0
 
-for i in set(permutations("sotocka")):
+for i in set(permutations("соточка")):
     s = "".join(i)
-    for j in "oa":
+    for j in "оа":
         s = s.replace(j, "x")
-    if "xx" not in s:
+    if "xx" in s:
         count += 1
 print(count)
