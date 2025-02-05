69319df28aaa53a9040489ad05a8737bfba6b3d0
Роман <rmanuylov.r@gmail.com>
2025-02-05 19:50:11 +0200

    75600

--- DZshkas/from 4-2-25/task 8 - 5300.py
+++ DZshkas/from 4-2-25/task 8 - 5300.py
@@ -1,9 +1,12 @@
 from itertools import permutations
 
 count = 0
-for i in set(permutations("hocu v vuz")):
-    s = "".join(i).split()
-    c1 = all(j[0] != "u" for j in s)
-    c2 = len(s) >= 2
-    if c1 & c2: count += 1
+for i in set(permutations("хочу в вуз")):
+    s = "".join(i)
+    c1 = (s == s.strip())
+    c2 = "  " not in s
+    s = s.split()
+    c3 = all(j[0] != "у" for j in s)
+    c4 = len(s) >= 2
+    if c1 & c2 & c3 & c4: count += 1
 print(count)
