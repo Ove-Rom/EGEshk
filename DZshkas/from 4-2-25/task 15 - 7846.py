1555ac3d323266728c23ccd8962832fc2ca1ff35
Роман <rmanuylov.r@gmail.com>
2025-02-05 19:35:56 +0200

    1

---
+++ DZshkas/from 4-2-25/task 15 - 7846.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+
+def f (x):
+    p = 13 <= x < 19
+    q = 17 <= x <= 23
+    a = a1 <= x <= a2
+    return (not((not p) <= q)) <= (a <= ((not q) <= p))
+
+line = [x/10 for x in range(120, 140)]
+ans = []
+
+for a1, a2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(a2 - a1)
+
+print(max(ans))
