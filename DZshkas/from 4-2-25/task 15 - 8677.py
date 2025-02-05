d6fa8453603f60b1cc9265d090ea61156d576b0b
Роман <rmanuylov.r@gmail.com>
2025-02-05 19:55:44 +0200

    114

--- DZshkas/from 4-2-25/task 15 - 8677.py
+++ DZshkas/from 4-2-25/task 15 - 8677.py
@@ -1,17 +1,4 @@
-from itertools import permutations
-
-
-def f(x):
-    b = 80 <= x <= 100
-    a = a1 <= x <= a2
-    return (x % 17 == 0) <= (not b or (a < x + 30))
-
-
-ans = []
-line = [x / 10 for x in range(790, 1010)]
-
-for a1, a2 in permutations(line, 2):
-    if all(f(x) for x in line):
-        ans.append(a2 - a1)
-
-print(max(ans))
+for a in range(1000, 0, -1):
+    if all((x% 17 == 0) <= (not (80 <= x <= 100) or (a < x + 30)) for x in range(1000)):
+        print(a)
+        break
