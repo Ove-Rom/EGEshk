e5fed2c9984a4fb2520b327e888d9ef6bff1273a
Роман <rmanuylov.r@gmail.com>
2025-02-07 16:12:07 +0200

    75599

--- DZshkas/from 4-2-25/task 8 - 5300.py
+++ DZshkas/from 4-2-25/task 8 - 5300.py
@@ -5,8 +5,8 @@ for i in set(permutations("хочу в вуз")):
     s = "".join(i)
     c1 = (s == s.strip())
     c2 = "  " not in s
-    s = s.split()
-    c3 = all(j[0] != "у" for j in s)
-    c4 = len(s) >= 2
-    if c1 & c2 & c3 & c4: count += 1
+    x = s.split()
+    c3 = all(j[0] != "у" for j in x)
+    c4 = len(x) >= 2
+    if c1 & c2 & c3 & c4 and s != "хочу в вуз": count += 1
 print(count)
