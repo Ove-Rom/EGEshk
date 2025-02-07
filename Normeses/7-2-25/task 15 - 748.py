757816b2b5548dad03d1d83ae83015a496cdbbf8
Роман <rmanuylov.r@gmail.com>
2025-02-07 16:57:25 +0200

    25

--- Normeses/7-2-25/task 15 - 748.py
+++ Normeses/7-2-25/task 15 - 748.py
@@ -5,11 +5,11 @@ def f(x):
     p = 44 <= x <= 49
     q = 28 <= x <= 53
     a = a1 <= x <= a2
-    return (a <= p) or q
+    return a <= p or q
 
 
 ans = []
-line = [x // 5 for x in range(43 * 5, 54 * 5)]
+line = [x / 5 for x in range(125, 275)]
 
 for a1, a2 in combinations(line, 2):
     if all(f(x) for x in line):
