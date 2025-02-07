87aad8b5a0ca581c8039ae73b13f5aee83a276ec
Роман <rmanuylov.r@gmail.com>
2025-02-07 16:51:13 +0200

    10

---
+++ Normeses/7-2-25/task 15 - 748.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+
+
+def f(x):
+    p = 44 <= x <= 49
+    q = 28 <= x <= 53
+    a = a1 <= x <= a2
+    return (a <= p) or q
+
+
+ans = []
+line = [x // 5 for x in range(43 * 5, 54 * 5)]
+
+for a1, a2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(a2 - a1)
+
+print(max(ans))
