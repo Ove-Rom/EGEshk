a38c5000f863c6725b29af70886be05c609d9e0c
Роман <rmanuylov.r@gmail.com>
2025-02-07 17:11:50 +0200

    29

---
+++ Normeses/7-2-25/task 15 - 3736.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+
+
+def f(x):
+    p = 117 <= x <= 158
+    q = 129 <= x <= 180
+    a = a1 <= x <= a2
+    return p <= ((q and not a) <= (not p))
+
+
+ans = []
+line = [x / 5 for x in range(115 * 5, 185 * 5)]
+
+for a1, a2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(a2 - a1)
+
+print(min(ans))
