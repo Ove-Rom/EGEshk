5dfb9b73705655931d8b2764d11e9b240362c001
Роман <rmanuylov.r@gmail.com>
2025-02-11 16:09:11 +0200

    175 173738

--- Normeses/11-02-25/task 17 - 4597/task 17 - 4597.py
+++ Normeses/11-02-25/task 17 - 4597/task 17 - 4597.py
@@ -1,5 +1,4 @@
-with open("17_4597.txt") as f:
-    data = [int(i) for i in f]
+with open("17_4597.txt") as f: data = [int(i) for i in f]
 
 minN = min(data)
 
@@ -7,8 +6,8 @@ count = 0
 maxCount = 0
 
 for i in range(len(data) - 1):
-    if data[i] % 117 == minN or data[i+1] % 117 == minN:
-        count+= 1
-        maxCount = max(maxCount, data[i]+data[i+1])
+    if data[i] % 117 == minN or data[i + 1] % 117 == minN:
+        count += 1
+        maxCount = max(maxCount, data[i] + data[i + 1])
 
 print(count, maxCount)
