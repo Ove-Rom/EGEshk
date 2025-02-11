5f2335edb2bc2227bacd75780e63d3b43ff7dff5
Роман <rmanuylov.r@gmail.com>
2025-02-11 16:34:03 +0200

    299 260264

--- Normeses/11-02-25/task 17 - 9748/task 17 - 9748.py
+++ Normeses/11-02-25/task 17 - 9748/task 17 - 9748.py
@@ -1,6 +1,6 @@
 with open("17_9748.txt") as f: data = [int(i) for i in f]
 
-minN = min(i for i in data if i % 100 == 15)
+maxN = max(i for i in data if i % 100 == 15)
 
 count = 0
 maxCount = 0
@@ -10,7 +10,7 @@ for i in range(len(data) - 2):
     c2 = len(str(data[i + 1])) == 4
     c3 = len(str(data[i + 2])) == 4
     if c1 ^ c2 ^ c3 and not (c1 & c2 & c3):
-        if sum(data[i:i + 3]) >= minN:
+        if sum(data[i:i + 3]) >= maxN:
             count += 1
             maxCount = max(maxCount, sum(data[i:i + 4]))
 
