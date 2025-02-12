f884f62ac9fe9dcdc95a448a8040447e42726bd0
Роман <rmanuylov.r@gmail.com>
2025-02-12 12:39:55 +0200

    52

--- DZshkas/from 11-2-25/task 15 - 18185.py
+++ DZshkas/from 11-2-25/task 15 - 18185.py
@@ -1,4 +1,4 @@
 for a in range(1000, -1000, -1):
-    if all(((x % 7) and (x % 17 == 0)) <= (x > a - 40) for x in range(1000)):
+    if all((x % 7 == 0) or (x % 13) or (x > a - 40) for x in range(1000)):
         print(a)
         break
