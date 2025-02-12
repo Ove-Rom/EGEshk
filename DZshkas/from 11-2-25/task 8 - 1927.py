103bfcf22853504553cd62265f650833ad6fcdd2
Роман <rmanuylov.r@gmail.com>
2025-02-12 12:26:38 +0200

    3240

---
+++ DZshkas/from 11-2-25/task 8 - 1927.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+count = 0
+
+for i in product("сновидец", repeat=5):
+    if i[0] in "снвдц" and i[-1] in "яоие":
+        if i.count(i[0]) == 1 == i.count(i[-1]):
+            count += 1
+
+print(count)
