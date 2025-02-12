016936874d550d3afe18e4988ef56619436468d7
Роман <rmanuylov.r@gmail.com>
2025-02-12 12:37:41 +0200

    6860

--- DZshkas/from 11-2-25/task 8 - 1927.py
+++ DZshkas/from 11-2-25/task 8 - 1927.py
@@ -2,7 +2,7 @@ from itertools import product
 
 count = 0
 
-for i in product("сновидец", repeat=5):
+for i in product("ясновидец", repeat=5):
     if i[0] in "снвдц" and i[-1] in "яоие":
         if i.count(i[0]) == 1 == i.count(i[-1]):
             count += 1
