b2e264616efe453fb1d719298c35b49ad97afb31
Роман <rmanuylov.r@gmail.com>
2025-02-14 16:27:31 +0200

    166

--- Normeses/14-2-25/task 5 - 9736.py
+++ Normeses/14-2-25/task 5 - 9736.py
@@ -1,10 +1,10 @@
 ans = []
 
-for n in range(1000):
+for n in range(10000):
     r = bin(n)[2:]
     if n % 3 == 0: r += r[-3:]
     else: r += bin((n % 3) * 3)[2:]
-    r = int(r)
+    r = int(r, 2)
     if r <= 170:
         ans.append(r)
 
