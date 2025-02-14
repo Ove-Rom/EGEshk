71a17ca939920e43d5680dd1f3c0640065150cbf
Роман <rmanuylov.r@gmail.com>
2025-02-14 19:38:29 +0200

    56 18612

--- Normeses/14-2-25/task 17 - 7716/task 17 - 7716.py
+++ Normeses/14-2-25/task 17 - 7716/task 17 - 7716.py
@@ -1,6 +1,6 @@
 with open("17_7716.txt") as f: data = [int(i) for i in f]
 
-maxN = max(i for i in data if all(int(j) % 2 for j in str(i)))
+maxN = max(i for i in data if all(int(j) % 2 != 0 for j in str(i)))
 ans = []
 
 for i in range(len(data) - 1):
@@ -9,4 +9,4 @@ for i in range(len(data) - 1):
     c3 = data[i] + data[i+1] > maxN
     if (c1 or c2) and c3: ans.append(data[i] + data[i+1])
 
-print(len(data), max(data))
+print(len(ans), max(ans))
