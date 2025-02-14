df2af933c2a3e20fd61b5eb3cdb0d039a76475a5
Роман <rmanuylov.r@gmail.com>
2025-02-14 20:09:20 +0200

    w x y z

---
+++ DZshkas/ULTRAprobnik/task 2.py
@@ -0,0 +1,15 @@
+from itertools import product, permutations
+
+
+def f(x, y, z, w):
+    return (x != (w and not z)) and (y == (x and not w))
+
+
+for q, w, e, r, t, y in product([0, 1], repeat=6):
+    tab = [(q, w, 0, e),
+           (r, 0, t, 0),
+           (0, y, 1, 0)]
+    if len(tab) == len(set(tab)):
+        for p in permutations("xyzw"):
+            if all(f(**dict(zip(p, t))) for t in tab):
+                print(*p)
