c5ed0f6ee0e365f8ce15d545d7df7bcd75bb19ad
Роман <rmanuylov.r@gmail.com>
2025-02-14 17:33:05 +0200

    1131

---
+++ Normeses/14-2-25/task - 14 - 4326.py
@@ -0,0 +1,19 @@
+from string import digits, ascii_lowercase
+
+alph = digits + ascii_lowercase
+def toq(a, q):
+    ans = ''
+    while a:
+        ans += alph[a%q]
+        a //= q
+    return ans[::-1]
+
+n = 3 * 15**1140 + 2 * 15**1025 + 15**923 - 3 * 15**85 + 2 * 15 ** 74 + 3
+n = toq(n, 15)
+
+c = 0
+
+for i in range(len(n) - 1):
+    if n[i] == n[i+1]: c += 1
+
+print(c)
