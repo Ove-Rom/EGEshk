dc9e556ea1b2164722a6b7749817ace837988f0d
Роман <rmanuylov.r@gmail.com>
2025-02-15 17:28:24 +0200

    19) 28 20) 25 52 21) 33

--- DZshkas/ULTRAprobnik/tasks 19, 20, 21.py
+++ DZshkas/ULTRAprobnik/tasks 19, 20, 21.py
@@ -2,14 +2,14 @@ def f(a, b, n):
     if a * b >= 777: return n % 2 == 0
     if n == 0: return 0
     h = [f(a + 3, b, n - 1), f(a, b + 3, n - 1),
-         f(a * 3, b, n - 1), f(a, b * 3, n - 1)]
+         f(a * 2, b, n - 1), f(a, b * 2, n - 1)]
     return all(h) if n % 2 == 0 else any(h)
 
 def f19(a, b, n):
     if a * b >= 777: return n % 2 == 0
     if n == 0: return 0
     h = [f(a + 3, b, n - 1), f(a, b + 3, n - 1),
-         f(a * 3, b, n - 1), f(a, b * 3, n - 1)]
+         f(a * 2, b, n - 1), f(a, b * 2, n - 1)]
     return any(h)
 
 print("19)", *[s for s in range(1, 111) if f19(7, s, 2)])
