2f5eed01cf6034e8bff85cdc37d3cf6e01a6ceb8
Роман <rmanuylov.r@gmail.com>
2025-02-15 10:22:42 +0200

    4194304

---
+++ DZshkas/ULTRAprobnik/task 16.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+
+@lru_cache(None)
+def f(n):
+    if n < 5: return 4
+    if n > 4: return 4 * f(n - 4)
+
+
+for i in range(4445):
+    f(i)
+
+print(f(4444) / f(4400))
