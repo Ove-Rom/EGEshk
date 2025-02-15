f06b64dcde1f49737073ffb485e619acb31973ac
Роман <rmanuylov.r@gmail.com>
2025-02-15 15:09:00 +0200

    54

--- DZshkas/ULTRAprobnik/task 24/task 24.py
+++ DZshkas/ULTRAprobnik/task 24/task 24.py
@@ -6,7 +6,7 @@ ans = []
 for i in range(100000):
     s = "RSQ" * i
     if s + "RSQ" not in data:
-        for j in combinations(["", "Q", "SQ", "RSQ" * i, "RS", "S", ""], 3):
+        for j in combinations(["", "Q", "SQ", "RSQ" * i, "RS", "R", ""], 3):
             j = ''.join(j)
             if j in data: ans.append(len(j))
         print(max(ans))
