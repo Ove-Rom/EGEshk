2ca192817c40a5a1a964489c34691a2288450e49
Роман <rmanuylov.r@gmail.com>
2025-02-15 14:43:48 +0200

    54

---
+++ DZshkas/ULTRAprobnik/task 24/task 24.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+data = open("24.txt").read()
+
+ans = []
+for i in range(100000):
+    s = "RSQ" * i
+    if s + "RSQ" not in data:
+        for j in combinations(["Q", "SQ", "RSQ" * i, "RS", "S"], 3):
+            j = ''.join(j)
+            if j in data: ans.append(len(j))
+        print(max(ans))
+        exit(80085)
