9b649a69438ec64f2589e6e5b1c54ef0b0e92d92
Роман <rmanuylov.r@gmail.com>
2025-02-21 16:58:58 +0200

    122157574 918478 1021575394 7681018 1421575554 10688538 1821575714 13696058

--- Normeses/21-2-25/task 25 - 11955.py
+++ Normeses/21-2-25/task 25 - 11955.py
@@ -1,9 +1,8 @@
+from fnmatch import fnmatch
+
 for i in range(1021706, 10 ** 10, 133):
     s = str(i)
-    c1 = s[0] == "1"
-    c2 = int(s[1]) % 2 == 0
-    c3 = s[2:6] == "2157"
-    c4 = all(int(j) % 2 != 0 for j in s[6:-1])
-    c5 = s[-1] == '4'
-    if c1 and c2 and c3 and c4 and c5:
+    c1 = int(s[1]) % 2 == 0
+    c2 = all(int(j) % 2 != 0 for j in s[6:-1])
+    if c1 and c2 and fnmatch(s, "1?2157*4"):
         print(i, i // 133)
