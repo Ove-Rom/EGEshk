0714b634bf2c21a19db8a17a4b691dcf53ac296e
Роман <rmanuylov.r@gmail.com>
2025-02-25 16:29:12 +0200

    195

---
+++ Normeses/25-2-25/task 5 - 15317.py
@@ -0,0 +1,10 @@
+for n in range(1, 10**10):
+    r = bin(n)[2:]
+    if n % 2 == 0:
+        r += "01"
+    else:
+        r = '1' + r + '1'
+    r = int(r, 2)
+    if r > 156:
+        print(r)
+        break
