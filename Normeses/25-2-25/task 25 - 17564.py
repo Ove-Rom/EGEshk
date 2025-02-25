ada5ffeadb94079c64b8b445a3ff9add74b48938
Роман <rmanuylov.r@gmail.com>
2025-02-25 17:07:47 +0200

    700000 175004 700003 700004 700004 350004 700005 10214 700008 116674

---
+++ Normeses/25-2-25/task 25 - 17564.py
@@ -0,0 +1,14 @@
+def div(x):
+    for i in range(2, round(x) + 1):
+        if x % i == 0 and str(i + x // i)[-1] == '4':
+            return i + x // i
+    return 0
+
+c = 0
+
+for i in range(700_000, 10**10):
+    m = div(i)
+    if m:
+        print(i, m)
+        c += 1
+        if c == 5: break
