42328e33ab174364a9f45dc195d011ecb39fdeb4
Роман <rmanuylov.r@gmail.com>
2025-02-28 17:27:25 +0200

    34 10 2962963 14 1793722 30 21052632 62 754717 14 1286 10 9 10

---
+++ Normeses/28-2-25/task 25 - 4276.py
@@ -0,0 +1,16 @@
+def div(x):
+    ans = set()
+    for i in range(2, round(x ** .5)):
+        if x % i == 0:
+            ans |= {i, x // i}
+    if len(ans) >= 7: return [sorted(ans)[-7], len(ans)]
+    return 0
+
+c = 0
+
+for i in range(400_000_001, 10**10):
+    d = div(i)
+    if d:
+        print(*d)
+        c += 1
+        if c == 7: break
