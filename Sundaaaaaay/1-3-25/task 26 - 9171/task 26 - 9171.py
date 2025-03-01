347940095e729fb1f672e8e618fa8a82c1f3a9ec
Роман <rmanuylov.r@gmail.com>
2025-03-01 18:02:20 +0200

    2873 267

--- Sundaaaaaay/1-3-25/task 26 - 9171/task 26 - 9171.py
+++ Sundaaaaaay/1-3-25/task 26 - 9171/task 26 - 9171.py
@@ -1,5 +1,4 @@
 with open("26_9171.txt") as f:
-    # with open("test.txt") as f:
     m, k, n = map(int, f.readline().split())
     data = [list(map(int, i.split())) for i in f]
 
@@ -7,10 +6,8 @@ base = dict()
 data = sorted(data, key=lambda x: (x[0], -x[1]))
 
 for i in data:
-    if i[0] not in base:
-        base[i[0]] = [i[1]]
-    else:
-        base[i[0]].append(i[1])
+    if i[0] not in base:base[i[0]] = [i[1]]
+    else:base[i[0]].append(i[1])
 
 free = k
 inTrain = []
@@ -21,14 +18,10 @@ for station in range(1, m):
     while station in inTrain: inTrain.remove(station); free += 1; haypuyushiye += 1
     if station in base:
         for stop in base[station]:
-            if free:
-                inTrain.append(stop)
-                free -= 1
+            if free: inTrain.append(stop); free -= 1
     if not free:
-        for i in range(station, m):
-            full[i] = 1
+        for i in range(station, m): full[i] = 1
     if free:
-        for i in range(station, m):
-            full[i] = 0
+        for i in range(station, m): full[i] = 0
 
 print(haypuyushiye, sum(full))
