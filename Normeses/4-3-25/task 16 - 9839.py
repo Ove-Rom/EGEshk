49157dbaca7fc0c54288d731f5255c838d0573e8
Роман <rmanuylov.r@gmail.com>
2025-03-04 17:03:15 +0200

    12114

---
+++ Normeses/4-3-25/task 16 - 9839.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+
+@lru_cache(None)
+def f(n):
+    if n < 3: return 3
+    if n >= 3: return 2 * n + 5 + f(n - 2)
+
+
+for i in range(3028): f(i)
+
+print((f(3027) - f(3023)))
