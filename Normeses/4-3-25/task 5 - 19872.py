d9925c7bd61cf50aadb7593c243ff1222bf0b03a
Роман <rmanuylov.r@gmail.com>
2025-03-04 16:11:23 +0200

    47

---
+++ Normeses/4-3-25/task 5 - 19872.py
@@ -0,0 +1,15 @@
+def toq(a, q):
+    ans = ''
+    while a:
+        ans += str(a % q)
+        a //= q
+    return ans
+
+for n in range(1000, 0, -1):
+    r = toq(n, 7)
+    if n % 2 == 0: r = "52" + r + '1'
+    else: r = r[-1] + r[1:-1] + r[0] + "15"
+    r = r.strip('0')
+    if len(r) == 4:
+        print(n)
+        break
