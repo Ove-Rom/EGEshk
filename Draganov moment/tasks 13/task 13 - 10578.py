86ab94efe2bbb9006c4d87f14f81441919545185
Роман <rmanuylov.r@gmail.com>
2025-03-11 15:12:54 +0200

    13

---
+++ Draganov moment/tasks 13/task 13 - 10578.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+ans = []
+
+for mask in range(33):
+    net1 = ip_network(f"10.96.180.231/{mask}", 0)
+    net2 = ip_network(f"10.96.140.118/{mask}", 0)
+    if str(net1.network_address) != str(net2.network_address) and\
+        bin(int(net1.netmask))[2:].count('1') == bin(int(net2.netmask))[2:].count('1'):
+        ans.append(bin(int(net1.netmask))[2:].count('0'))
+
+print(max(ans))
