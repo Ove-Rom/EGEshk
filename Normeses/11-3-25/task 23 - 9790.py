c151b77f537129ae8fe12042c85e5aef2923b767
Роман <rmanuylov.r@gmail.com>
2025-03-11 17:04:54 +0200

    180

--- Normeses/11-3-25/task 23 - 9790.py
+++ Normeses/11-3-25/task 23 - 9790.py
@@ -2,6 +2,6 @@ def z(o, v):
     if o in (9, 16): return 0
     if o == v: return 1
     if o < v: return 0
-    return z(o - 2, v) + z(o - 2, v) + z(o // 3, v)
+    return z(o - 1, v) + z(o - 2, v) + z(o // 3, v)
 
 print(z(19, 3))
