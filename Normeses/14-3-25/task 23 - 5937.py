2151347487a29daec3e99e6132f214e795f122b3
Роман <rmanuylov.r@gmail.com>
2025-03-14 17:29:23 +0200

    4197234

--- Normeses/14-3-25/task 23 - 5937.py
+++ Normeses/14-3-25/task 23 - 5937.py
@@ -1,9 +1,9 @@
 def f(a, b, c = 0):
+    if a % 2 == 0: c += 1
     if a == b:
         if c <= 15: return 1
         return 0
     if a > b: return 0
-    if a % 2 == 0: c += 1
-    return f(a + 2, b, c) + f( a + 3, b, c) + f(a * 2 + 1, b, c)
+    return f(a + 2, b, c) + f(a + 3, b, c) + f(a * 2 + 1, b, c)
 
-print(f(11, 55))
+print(f(1, 55))
