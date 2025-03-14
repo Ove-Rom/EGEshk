9b1fb79c12e55581f37357bc04f9ea4e04fc534c
Роман <rmanuylov.r@gmail.com>
2025-03-14 17:43:42 +0200

    130771

--- Normeses/14-3-25/task 23 - 5762.py
+++ Normeses/14-3-25/task 23 - 5762.py
@@ -1,9 +1,12 @@
 pattern = [i**2 for i in range(10)]
 
+ans = [0] * 100
+
 def f(a, b, c):
-    if a == b: return 1
+    if a == b: ans[c+1] += 1
     if a > b: return 0
     if a in pattern: c += 1; a -= 1
     return f(a + 2, b, c) + f(a + 3, b, c) + f(a * 3, b, c)
 
-print(f(5, 50, 0))
+f(5, 50, 0)
+print(max(ans[1:]))
