809fb2aa980aaa7fa390901af2ea99c493fca140
Роман <rmanuylov.r@gmail.com>
2025-03-14 16:25:05 +0200

    681

--- Normeses/14-3-25/task 12 - 19483.py
+++ Normeses/14-3-25/task 12 - 19483.py
@@ -1,9 +1,9 @@
 for i in range(4, 10000):
     s = '2' + '5' * i
     while "25" in s or "355" in s or "555" in s:
-        s=s.replace("25", '5')
-        s=s.replace("355", "552")
-        s=s.replace("555", '3')
+        s=s.replace("25", '5', 1)
+        s=s.replace("355", "522", 1)
+        s=s.replace("555", '3', 1)
     if s.count('2') == 10:
         print(i)
         break
