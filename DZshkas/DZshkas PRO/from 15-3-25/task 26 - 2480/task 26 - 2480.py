18527105e23bec1750eff57c80b060df18e96db1
Роман <rmanuylov.r@gmail.com>
2025-03-22 17:52:55 +0200

    46864 187392

--- DZshkas/DZshkas PRO/from 15-3-25/task 26 - 2480/task 26 - 2480.py
+++ DZshkas/DZshkas PRO/from 15-3-25/task 26 - 2480/task 26 - 2480.py
@@ -1,7 +1,7 @@
 roadToRepair = set()
 
 with open("26_2480.txt") as f:
-    # with open("test") as f:
+# with open("test") as f:
     n = int(f.readline())
     for i in f:
         roadToRepair.update(set(range(int(i.split()[0]), int(i.split()[1]) + 1)))
