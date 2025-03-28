5ce4cc936a89c9bad6475d6ca56d7ab6ea49cc8d
Роман <rmanuylov.r@gmail.com>
2025-03-28 16:19:48 +0200

    1273552 229 1273585 1607 1273596 719 1273606 863 1273608 31

---
+++ Normeses/28-3-25/task 25 - 8696.py
@@ -0,0 +1,21 @@
+def isP(x):
+    for i in range(2, round(x ** .5) + 1):
+        if x % i == 0: return 0
+    return 1
+
+def div(x):
+    ans = {0}
+    for i in range(2, round(x ** .5) + 1):
+        if x % i == 0:
+            if isP(x // i): ans.add(x // i)
+            if isP(i): ans.add(i)
+    return sum(ans)
+
+c = 0
+
+for i in range(1_273_547, 10**10):
+    m = div(i)
+    if m and isP(m % 100_000):
+        print(i, m)
+        c += 1
+        if c == 5: break
