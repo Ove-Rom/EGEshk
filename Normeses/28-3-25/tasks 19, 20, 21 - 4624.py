11e3539357aaacf6705a59634db33893fbdcedec
Роман <rmanuylov.r@gmail.com>
2025-03-28 16:39:46 +0200

    19) 82 20) 41 81 21) 80

---
+++ Normeses/28-3-25/tasks 19, 20, 21 - 4624.py
@@ -0,0 +1,9 @@
+def f(a, n):
+    if a >= 165: return n % 2 == 0
+    if n == 0: return 0
+    h = [f(a + 1, n - 1), f(a * 2, n - 1)]
+    return all(h) if n % 2 == 0 else any(h)
+
+print("19)", *[a for a in range(1, 165) if f(a, 2)])
+print("20)", *[a for a in range(1, 165) if f(a, 3) and not f(a, 1)])
+print("21)", *[a for a in range(1, 165) if f(a, 4) and not f(a, 2)])
