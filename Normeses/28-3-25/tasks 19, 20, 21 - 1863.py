47966cb017d9b69d3e210d0d21662c3e799c5c8d
Роман <rmanuylov.r@gmail.com>
2025-03-28 16:40:40 +0200

    19) 19 20) 15 18 21) 14 17

--- Normeses/28-3-25/tasks 19, 20, 21 - 1863.py
+++ Normeses/28-3-25/tasks 19, 20, 21 - 1863.py
@@ -4,6 +4,6 @@ def f(a, n):
     h = [f(a + 1, n - 1), f(a + 4, n - 1), f(a * 2, n - 1)]
     return all(h) if n % 2 == 0 else any(h)
 
-print("19)", *[a for a in range(1, 40) if f(a, 2) and not f(a, 1)])
+print("19)", *[a for a in range(1, 40) if f(a, 2)])
 print("20)", *[a for a in range(1, 40) if f(a, 3) and not f(a, 1)])
 print("21)", *[a for a in range(1, 40) if f(a, 4) and not f(a, 2)])
