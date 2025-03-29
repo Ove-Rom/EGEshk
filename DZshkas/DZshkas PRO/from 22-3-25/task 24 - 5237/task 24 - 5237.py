87b27f4a349ee6c28abc902c9704678213bb5278
Роман <rmanuylov.r@gmail.com>
2025-03-29 19:00:55 +0200

    15

--- DZshkas/DZshkas PRO/from 22-3-25/task 24 - 5237/task 24 - 5237.py
+++ DZshkas/DZshkas PRO/from 22-3-25/task 24 - 5237/task 24 - 5237.py
@@ -4,6 +4,15 @@ data = open("24_5237.txt").read()
 
 data = data.replace('Z', ' ').split()
 
-patttern = "XY" * 10**5
+# patttern = "XY" * 10**5
+#
+# print(max(len(i) for i in data if i in patttern))
 
-print(max(len(i) for i in data if i in patttern))
+l = []
+
+for i in data:
+    if fullmatch(r"Y?(XY)*X?", i) or \
+            fullmatch(r"X?(YX)*Y?", i):
+        l.append(i)
+
+print(len(max(l, key=len)))
