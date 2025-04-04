e2ad3e9cbf72214e52179b8ea1dcc79e6cad54b4
Роман <rmanuylov.r@gmail.com>
2025-04-04 17:38:54 +0200

    36

--- Normeses/4-4-25/task 9 - 9740/task 9 - 9740.py
+++ Normeses/4-4-25/task 9 - 9740/task 9 - 9740.py
@@ -5,7 +5,7 @@ c = 0
 for i in d:
     np = [j for j in i if i.count(j) == 1]
     p = [j for j in i if i.count(j) != 1]
-    c1 = len(p) == 3
+    c1 = len(p) == 3 and len(set(p)) == 1
     c2 = sum(np) / 4 <= sum(p) // 3
     if c1 and c2:
         c += 1
