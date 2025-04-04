4f8eedcbf8b917ccb2b9b71e6b45aa9dfa28e0ad
Роман <rmanuylov.r@gmail.com>
2025-04-04 17:33:47 +0200

    36

--- Normeses/4-4-25/task 9 - 9740/task 9 - 9740.py
+++ Normeses/4-4-25/task 9 - 9740/task 9 - 9740.py
@@ -3,10 +3,10 @@ d = [tuple(map(int, i.split())) for i in open("9_9740.txt")]
 c = 0
 
 for i in d:
-    np = {j for j in i if i.count(j) == 1}
-    p = set(i) - np
-    c1 = len(p) == 1
-    c2 = sum(np) / len(np) <= sum(p)
+    np = [j for j in i if i.count(j) == 1]
+    p = [j for j in i if i.count(j) != 1]
+    c1 = len(p) == 3
+    c2 = sum(np) / 4 <= sum(p) // 3
     if c1 and c2:
         c += 1
 
