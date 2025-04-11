188698d14e3cca8f7dcda0fa3bc62074cf15cafd
Роман <rmanuylov.r@gmail.com>
2025-04-11 16:28:17 +0200

    1001

---
+++ Normeses/11-4-25/task 13 - 17526.py
@@ -0,0 +1,12 @@
+from ipaddress import *
+
+net = ip_network("172.16.128.0/255.255.192.0")
+
+c = 0
+
+for ip in net:
+    ip = bin(int(ip))[2:]
+    if ip.count('1') == ip.count('0'):
+        c += 1
+
+print(c)
