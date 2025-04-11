cd2d9211f3c7e8253899d95ffb5a7c9e5a6de6f9
Роман <rmanuylov.r@gmail.com>
2025-04-11 17:02:30 +0200

    252

--- Normeses/11-4-25/task 13 - 11790.py
+++ Normeses/11-4-25/task 13 - 11790.py
@@ -1,7 +1,8 @@
 from ipaddress import *
 
-for a in range(16, 25):
-    net = ip_network(f"152.65.245.132/{a}", False)
+for a in range(1, 9):
+    # print(int(f"{'1'*a:08}", 2))
+    net = ip_network(f"152.65.245.132/255.255.{int(f"{'1'*a:08}", 2)}.0", False)
     if all(f"{int(i):032b}"[:17].count("0") >= f"{int(i):032b}"[17:].count("0") for i in net):
-        print(a)
+        print(int(f"{'1'*a:08}", 2))
         break
