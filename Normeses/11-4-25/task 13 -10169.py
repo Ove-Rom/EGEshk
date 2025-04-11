291e21ad3e8f498d2805c27bb7325d82546ea620
Роман <rmanuylov.r@gmail.com>
2025-04-11 17:36:57 +0200

    21

---
+++ Normeses/11-4-25/task 13 -10169.py
@@ -0,0 +1,8 @@
+from ipaddress import *
+
+for mask in range(33):
+    net1 = ip_network(f"157.127.182.76/{mask}", False)
+    net2 = ip_network(f"157.127.190.80/{mask}", False)
+    if net1.network_address != net2.network_address:
+        print(mask)
+        break
