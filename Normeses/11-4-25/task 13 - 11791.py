aa46af2c00dcd7d20817a16b34f2676fa601ec00
Роман <rmanuylov.r@gmail.com>
2025-04-11 17:11:27 +0200

    254

---
+++ Normeses/11-4-25/task 13 - 11791.py
@@ -0,0 +1,11 @@
+from ipaddress import *
+
+for a in range(16, 25):
+    net = ip_network(f"246.51.128.202/{a}", False)
+    for i in net:
+        b = f"{int(i):032b}"
+        if not (b[:17].count("0") <= b[17:].count("0")):
+            break
+    else:
+        print(f"{int(f"{'1' * (a-16):08}", 2)}")
+        break
