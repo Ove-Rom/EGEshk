63888a946e3643fd371aa0a0c6c10c4e05ee98fe
Роман <rmanuylov.r@gmail.com>
2025-04-12 19:32:41 +0200

    78

---
+++ Sundaaaaaay/12-4-25/task 24 - 17563/task 24 - 19149/task 24 - 19149.py
@@ -0,0 +1,12 @@
+from re import finditer
+
+with open("24_19149.txt") as f:
+    data = f.read()
+
+p = r"[(](\d+|([(]\d+[+]\d+[)]))([+](\d+|([(]\d+[+]\d+[)])))*[)]"
+
+base = finditer(p, data)
+
+lines = [i.group() for i in base]
+print(lines)
+print(len(max(lines, key=lambda x: len(x) if eval(x) % 2 == 0 else 0)))
