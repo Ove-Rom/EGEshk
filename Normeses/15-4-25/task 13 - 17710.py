8ef6d1737179d33f686542ff60cd0dea49f4c1e3
Роман <rmanuylov.r@gmail.com>
2025-04-15 16:21:50 +0200

    6764

---
+++ Normeses/15-4-25/task 13 - 17710.py
@@ -0,0 +1,15 @@
+from importlib.resources import read_text
+from ipaddress import *
+
+net = ip_network("214.187.224.0/255.255.224.0", False)
+
+c = 0
+
+for i in net:
+    s = f"{int(i):032b}"
+    c1 = s.count("1") % 6
+    c2 = s[-4:] != "1000"
+    if c1 and c2:
+        c += 1
+
+print(c)
