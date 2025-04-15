02c0749085ca4ef56cdd510107b301a31768b342
Роман <rmanuylov.r@gmail.com>
2025-04-15 16:52:47 +0200

    1562

--- DZshkas/HYPERprobnik/task 8 - 1363.py
+++ DZshkas/HYPERprobnik/task 8 - 1363.py
@@ -1,14 +1,1 @@
-def toq(a, q):
-    ans = ""
-    while a:
-        ans += str(a % q)
-        a //= q
-    return ans[::-1]
-
-c = 0
-
-for i in range(int("300001", 5), int("400000", 5), 2):
-    n = toq(i, 5)
-    if n[0] == "3": c += 1
-
-print(c)
+print(len(range(int("300001", 5), int("400000", 5), 2)))
