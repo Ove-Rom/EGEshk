1804e66d5f6731df4b16de564b2830096be68730
Роман <rmanuylov.r@gmail.com>
2025-04-18 08:19:39 +0200

    192

---
+++ DZshkas/🤙probnik/task 13 - 10151.py
@@ -0,0 +1,9 @@
+from ipaddress import *
+
+ip = ip_address("111.81.192.0")
+
+for mask in range(33):
+    net = ip_network(f"111.81.208.27/{mask}", False)
+    if net.network_address == ip:
+        print(str(net.netmask).split(".")[2])
+        break
