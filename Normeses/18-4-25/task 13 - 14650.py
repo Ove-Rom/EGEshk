fe7351edc8489ae64ea3b7a6451499d2bcc39233
Роман <rmanuylov.r@gmail.com>
2025-04-18 16:53:25 +0200

    24

--- Normeses/18-4-25/task 13 - 14650.py
+++ Normeses/18-4-25/task 13 - 14650.py
@@ -6,7 +6,8 @@ ip2 = ip_address("216.54.174.128")
 for mask in range(32, -1, -1):
     n1 = ip_network(f"{ip1}/{mask}", False)
     n2 = ip_network(f"{ip2}/{mask}", False)
-    if ip1 in n1.hosts() and ip2 in n2.hosts():
+    if (ip1 not in [n1.broadcast_address, n1.network_address] and
+        ip2 not in [n2.broadcast_address, n2.network_address]):
         if n1 != n2:
             print(mask)
             break
