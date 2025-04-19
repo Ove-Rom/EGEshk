a44a97985f6e3156fc11769c185a3bc6cf311803
Роман <rmanuylov.r@gmail.com>
2025-04-19 16:33:26 +0200

    45

---
+++ Sundaaaaaay/19-4-25/task 24 - 18957/task 24 - 18957.py
@@ -0,0 +1,14 @@
+from re import *
+with open("24_18597.txt") as f:
+    data = f.read()
+
+pattern = r"[1-9]\d{3}[.]\d+&\d{4}[.]\d+"
+
+lines = finditer(pattern, data)
+
+ans = []
+
+for i in lines:
+    ans.append(len(i.group()))
+
+print(max(ans))
