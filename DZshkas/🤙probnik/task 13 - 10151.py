dc6fbfecc5a6cc37ba6de3de1008714f1f0633ec
Роман <rmanuylov.r@gmail.com>
2025-04-21 18:49:17 +0200

    192

--- DZshkas/🤙probnik/task 13 - 10151.py
+++ DZshkas/🤙probnik/task 13 - 10151.py
@@ -1,9 +1,11 @@
 from ipaddress import *
 
-ip = ip_address("111.81.192.0")
+address = ip_address("111.81.192.0")
+ip = ip_address("111.81.208.27")
 
 for mask in range(33):
     net = ip_network(f"111.81.208.27/{mask}", False)
-    if net.network_address == ip:
+    if net.network_address == address and \
+            net.broadcast_address != ip:
         print(str(net.netmask).split(".")[2])
         break
