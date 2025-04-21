8dd4713e96e967a3e15e215c5c6a91001069efae
Роман <rmanuylov.r@gmail.com>
2025-04-21 19:15:00 +0200

    2471

--- DZshkas/🤙probnik/task 24 - 19717/task 24 - 19717.py
+++ DZshkas/🤙probnik/task 24 - 19717/task 24 - 19717.py
@@ -2,9 +2,8 @@ from tqdm import tqdm
 
 with open("24.5_19717.txt") as f:
     data = f.read()
-
 # data = "AAAMAASAMAAAAMAMMAAAAAAAAAAAM"
-k = 3
+k = 278
 
 data = data.split("M")
 
@@ -14,10 +13,10 @@ c = 0
 for i in tqdm(range(len(data) - k)):
     lens = []
     for j in range(k+1):
-        print(data[i+j])
+        # print(data[i+j])
         lens.append(len(data[i+j]) + 1)
     s = sum(lens)
     ans.append(s-1)
-    print(s-1)
+    # print(s-1)
 
 print(max(ans))
