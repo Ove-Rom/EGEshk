a6f22e1d6a5c6f2de03235ed64f681fa7aaaa2e6
Роман <rmanuylov.r@gmail.com>
2025-04-22 16:54:15 +0200

    1357

---
+++ Normeses/varik 22-4-25/task 1 - 9148.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = "аг гж жз зд дв ва ба бг бв гд еж ез ед".split()
+matrix = "256 1458 478 237 126 158 248 2367".split()
+
+print(*range(1, 9))
+
+for i in permutations("абвгдежз"):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
