0b3eced89ed4a1f19ec17b4d205bf85f707084dc
Роман <rmanuylov.r@gmail.com>
2025-04-22 17:29:21 +0200

    36720

--- Normeses/varik 22-4-25/task 8 - 19480.py
+++ Normeses/varik 22-4-25/task 8 - 19480.py
@@ -3,7 +3,8 @@ from itertools import permutations
 c = 0
 
 for i in set(permutations("парижанка")):
-    if sum(j in "".join(i) for j in ["иа", "аа"]) == 1:
+    s =  "".join(i)
+    if sum(j in s for j in ["иа", "аа"]) == 1:
         c += 1
 
 print(c)
