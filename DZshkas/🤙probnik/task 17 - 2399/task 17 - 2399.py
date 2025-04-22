5c3cd8a3277c6d0b73718cb68799e265e32abfcc
Роман <rmanuylov.r@gmail.com>
2025-04-22 16:20:38 +0200

    15 6410

--- DZshkas/🤙probnik/task 17 - 2399/task 17 - 2399.py
+++ DZshkas/🤙probnik/task 17 - 2399/task 17 - 2399.py
@@ -6,8 +6,8 @@ sumN = sum(sum(int(j) for j in str(i)) for i in data if i % 35 == 0)
 ans = []
 
 for n1, n2 in zip(data, data[1:]):
-    c1 = (n1 > sumN) and (hex(n2)[-2:] == "ef" and n2 <= sumN)
-    c2 = (n2 > sumN) and (hex(n1)[-2:] == "ef" and n1 <= sumN)
+    c1 = n1 > sumN >= n2 and hex(n2)[-2:] == "ef"
+    c2 = n2 > sumN >= n1 and hex(n1)[-2:] == "ef"
     if c1 ^ c2:
         ans.append(n1 + n2)
 
