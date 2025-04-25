e35ff92c22d46a13ff3c782cd27f4df041a3711b
Роман <rmanuylov.r@gmail.com>
2025-04-25 20:02:40 +0200

    z y x w

---
+++ DZshkas/DZshkas PRO/EGKR 19-4-25/task 2 - 21697.py
@@ -0,0 +1,14 @@
+from itertools import product, repeat, permutations
+
+
+def f(x, y, z, w):
+    return not(x <= y) or (z == w) or z
+
+for q,w,e,r,t,y,u in product([1, 0], repeat=7):
+    tab = [(0,0,q,w),
+           (e,r,1,t),
+           (y,1,0,u)]
+    if len(set(tab)) == len(tab):
+        for p in permutations("xyzw"):
+            if [0,0,0] == [f(**dict(zip(p, t))) for t in tab]:
+                print(*p)
