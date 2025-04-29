3566fd2744b946bf9138ae445461e5c5e506f9b5
Роман <rmanuylov.r@gmail.com>
2025-04-29 16:40:42 +0200

    170083346818 1965986 173010946738 1999826 175769846308 2031716 177402346618 2050586 177965546248 2057096 178697446228 2065556

--- Normeses/29-4-25/task 25 - 14438.py
+++ Normeses/29-4-25/task 25 - 14438.py
@@ -1,9 +1,9 @@
 from fnmatch import fnmatch
 
-sq = [i ** 2 for i in range(500)]
+sq = [i ** 2 for i in range(109)]
 
-for i in range(1746008 + 1746008 % 86513, 10**12, 86513):
+for i in range(1746008 - 1746008 % 86513, 10**12, 86513):
     if fnmatch(str(i), "17*46??8"):
         s = sum(int(j) for j in str(i))
         if s in sq:
-            print(i, s)
+            print(i, i // 86513)
