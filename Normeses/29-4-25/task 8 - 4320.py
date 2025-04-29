792a9eb47c6f20ce4ddf184daa872a397489993f
Роман <rmanuylov.r@gmail.com>
2025-04-29 16:08:19 +0200

    3554

--- Normeses/29-4-25/task 8 - 4320.py
+++ Normeses/29-4-25/task 8 - 4320.py
@@ -5,7 +5,7 @@ c = 0
 for i in product("01234567", repeat=6):
     s = "".join(i).lstrip("0")
     if s:
-        c1 = len(s) == 6
+        c1 = len(s) == len(set(s)) == 6
         l = [int(j) % 2 for j in s]
         c2 = not any(j in l for j in ["11", "00"])
         c3 = int(s, 8) % 5 == 0
