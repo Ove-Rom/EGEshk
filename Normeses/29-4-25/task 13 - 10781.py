0c2373952b4e2c7c094f91ee0a58702bfe3af052
Роман <rmanuylov.r@gmail.com>
2025-04-29 16:43:18 +0200

    4294967296

---
+++ Normeses/29-4-25/task 13 - 10781.py
@@ -0,0 +1,13 @@
+from ipaddress import *
+
+ip1 = ip_address("112.117.107.70")
+ip2 = ip_address("112.117.121.80")
+
+for mask in range(33):
+    net1 = ip_network(f"{ip1}/{mask}", False)
+    net2 = ip_network(f"{ip2}/{mask}", False)
+    if ip1 not in (net1.network_address, net1.broadcast_address) and \
+            ip2 not in (net2.network_address, net2.broadcast_address):
+        if net1 == net2:
+            print(net1.num_addresses)
+            break
