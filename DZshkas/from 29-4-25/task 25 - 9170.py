aa1ee413a3f5c791eba6a1bc95b443f940c528ae
Роман <rmanuylov.r@gmail.com>
2025-04-30 15:08:00 +0200

    997920 498960

--- DZshkas/from 29-4-25/task 25 - 9170.py
+++ DZshkas/from 29-4-25/task 25 - 9170.py
@@ -1,12 +1,16 @@
+from tqdm import tqdm
+
+
 def div(x):
     ans = set()
-    for i in (1, int(x ** .5) + 1):
+    for i in range(1, int(x ** .5) + 1):
         if x % i == 0:
             if str(i)[0] == "4": ans.add(i)
-            if str(x//i)[0] == "4": ans.add(x//i)
+            if str(x // i)[0] == "4": ans.add(x // i)
     if len(ans) == 24: return max(ans)
     return False
 
-for i in range(10**6):
-    if d:= div(i):
+
+for i in tqdm(range(10 ** 6)):
+    if d := div(i):
         print(i, d)
