898f9269b879cf286d1ed13e7c6a44a41672df37
Роман <rmanuylov.r@gmail.com>
2025-05-01 14:17:30 +0200

    1048576 0 2097152 0 3145728 3145725 4194304 0 5242880 5242875

--- DZshkas/from 29-4-25/task 25 - 5735.py
+++ DZshkas/from 29-4-25/task 25 - 5735.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 def div(x):
     ans = set()
     c = 0
+    flag = True
     if int(log2(x)) == log2(x): c += 1
     for i in range(2, int(x ** .5) + 1):
         if x % i == 0:
@@ -14,20 +15,22 @@ def div(x):
                 c += 1
             else:
                 ans.add(i)
+                flag = False
             if int(log2(d)) == log2(d):
                 c += 1
             else:
                 ans.add(d)
-    if c >= 20: return sum(ans)
-    return False
+                flag = False
+    if c >= 20:
+        if flag: return 0
+        return sum(ans)
+    return -1
 
 
 c = 0
 
 for i in tqdm(range(10 ** 6 + 1, 10 ** 7)):
-    d = div(i)
-    if d:
+    if (d := div(i)) + 1:
         print(i, d)
         c += 1
         if c == 5: break
-        # print("*****")
