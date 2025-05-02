a26013eb83ed5cd102eb3c1781ef04b96660170b
Роман <rmanuylov.r@gmail.com>
2025-05-02 22:27:14 +0200

    19) 39 20) 38 110 21) 108

--- DZshkas/from 2-5-25/tasks 19, 20, 21 - 8163.py
+++ DZshkas/from 2-5-25/tasks 19, 20, 21 - 8163.py
@@ -1,15 +1,15 @@
 def f(a, n):
     if a >= 348: return n % 2 == 0
     if n == 0: return 0
-    h = [f(a + 3, n - 1), f(a * 3, n - 1)]
+    h = [f(a + 4, n - 1), f(a + 2, n - 1), f(a * 3, n - 1)]
     return all(h) if n % 2 == 0 else any(h)
 
 
 def f19(a, n):
     if a >= 348: return n % 2 == 0
     if n == 0: return 0
-    h = [f19(a + 3, n - 1), f19(a * 3, n - 1)]
-    return any(h) 
+    h = [f19(a + 4, n - 1), f19(a + 2, n - 1), f19(a * 3, n - 1)]
+    return any(h)
 
 
 print("19)", *[a for a in range(1, 348) if f19(a, 2)])
