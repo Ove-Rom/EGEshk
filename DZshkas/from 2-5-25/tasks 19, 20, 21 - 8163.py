f760c00747f90eff3adee5427e7f9d5daa439c22
Роман <rmanuylov.r@gmail.com>
2025-05-02 20:59:51 +0200

    19) 39 20) 38 21) 107

---
+++ DZshkas/from 2-5-25/tasks 19, 20, 21 - 8163.py
@@ -0,0 +1,17 @@
+def f(a, n):
+    if a >= 348: return n % 2 == 0
+    if n == 0: return 0
+    h = [f(a + 3, n - 1), f(a * 3, n - 1)]
+    return all(h) if n % 2 == 0 else any(h)
+
+
+def f19(a, n):
+    if a >= 348: return n % 2 == 0
+    if n == 0: return 0
+    h = [f19(a + 3, n - 1), f19(a * 3, n - 1)]
+    return any(h)
+
+
+print("19)", *[a for a in range(3, 348) if f19(a, 2)])
+print("20)", *[a for a in range(3, 348) if f(a, 3) and not f(a, 1)])
+print("21)", *[a for a in range(3, 348) if f(a, 4) and not f(a, 2)])
