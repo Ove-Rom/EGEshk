e5b6df56b145fdabaa11027c4602d6f02bdf6f84
Роман <rmanuylov.r@gmail.com>
2025-05-09 17:53:04 +0200

    559397 625605 923413 613488

---
+++ Normeses/9-5-25/task 27 - 18630/task 27 - 18630.py
@@ -0,0 +1,33 @@
+from math import dist
+
+with open("27B_18630.txt") as f:
+    data = [list(map(float, i.split())) for i in f]
+
+def c(cl):
+    ans = []
+    for d1 in cl:
+        s = 0
+        for d2 in cl:
+            s += dist(d1, d2)
+        ans.append([s, d1])
+    return min(ans)[1]
+
+eps = 1
+clast = []
+
+while data:
+    cl = [data.pop(0)]
+    for d1 in cl:
+        for d2 in data.copy():
+            if dist(d1, d2) <= eps:
+                cl.append(d2)
+                data.remove(d2)
+    if len(cl) > 10: clast.append(cl)
+
+cent = [c(i) for i in clast]
+print(cent)
+
+px = int(sum(i[0] for i in cent) * 100_000 / len(cent))
+py = int(sum(i[1] for i in cent) * 100_000 / len(cent))
+
+print(px, py)
