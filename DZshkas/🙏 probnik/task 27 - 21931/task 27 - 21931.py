614707ca864c3f98c2d3391982e5bbf695444185
Роман <rmanuylov.r@gmail.com>
2025-05-11 15:26:44 +0200

    18049 111324 174474 142246

---
+++ DZshkas/🙏 probnik/task 27 - 21931/task 27 - 21931.py
@@ -0,0 +1,46 @@
+from math import dist
+
+# with open("27_A_21931.txt") as f:
+with open("27_B_21931.txt") as f:
+    data = [list(map(float, i.split())) for i in f]
+    # c1 = []
+    # c2 = []
+    # c3 = []
+    # for i in f:
+    #     x, y = map(float, i.split())
+        # if y < 10: c1.append([x, y])
+        # else: c2.append([x, y])
+        # if y < 10: c1.append([x, y])
+        # elif x < 17: c2.append([x, y])
+        # else: c3.append([x, y])
+
+# clast = [c1, c2, c3]
+
+def c(cl):
+    ans = []
+    for d1 in cl:
+        s = 0
+        for d2 in cl:
+            s += dist(d1, d2)
+        ans.append([s, d1])
+    return max(ans)[1]
+
+eps = 1
+clast = []
+
+while data:
+    cl = [data.pop()]
+    for d1 in cl:
+        for d2 in data.copy():
+            if dist(d1, d2) <= eps:
+                cl.append(d2)
+                data.remove(d2)
+    clast.append(cl)
+
+cent = [c(i) for i in clast]
+print(cent)
+
+px = int(sum(i[0] for i in cent) * 10_000 / len(cent))
+py = int(sum(i[1] for i in cent) * 10_000 / len(cent))
+
+print(px, py)
