d9cf25f7935bbd4f9bc0d1d838e78ab698eb9109
Роман <rmanuylov.r@gmail.com>
2025-05-11 15:40:41 +0200

    61236

--- DZshkas/🙏 probnik/task 8 - 3149.py
+++ DZshkas/🙏 probnik/task 8 - 3149.py
@@ -3,7 +3,8 @@ from itertools import product
 trip = [i*3 for i in "0123456789"]
 c = 0
 
-for i in product("0123456789", repeat=5):
+for i in range(10000, 100000):
+    i = str(i)
     if i[-1] in "347": continue
     if not any(j in "".join(i) for j in trip):
         c += 1
