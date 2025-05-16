1ad8d63b6fff6fefe0698c52be412a0b037d28d3
Роман <rmanuylov.r@gmail.com>
2025-05-16 16:37:51 +0200

    1023

--- Normeses/16-5-25/task 5 - 4869.py
+++ Normeses/16-5-25/task 5 - 4869.py
@@ -1,6 +1,6 @@
 for n in range(2, 10**10):
     r = f"{n:b}"
-    c1 = sum(j == "1" for j in r[1:2])
+    c1 = sum(j == "1" for j in r[1::2])
     c0 = sum(j == "0" for j in r[::2])
     r = abs(c1 - c0)
     if r == 5:
