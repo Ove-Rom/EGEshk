1ed4c5a33f23e2d771089ce4d62ac8b8aa149085
Роман <rmanuylov.r@gmail.com>
2025-05-16 17:51:10 +0200

    182

---
+++ Normeses/16-5-25/tasks 24 2024/proga.py
@@ -0,0 +1,45 @@
+from re import finditer
+# with open("24_17535.txt") as f:
+#     data = f.read()
+#
+# data = data.split("CD")
+#
+# ans = []
+#
+# for i in range(len(data) - 160):
+#     s = "".join(data[i:i+161])
+#     ans.append(len(s) + (160*2 + 2))
+#
+# print(max(ans))
+
+# with open("24_10105.txt") as f:
+#     data = f.read().split("T")
+#
+# ans = []
+#
+# for i in range(len(data) - 100):
+#     s = "".join(data[i:i+101])
+#     ans.append(len(s) + 100)
+#
+# print(max(ans))
+
+# with open("24_12254.txt") as f:
+#     data = f.read()
+#
+# pat = r"(SQ|Q)?(RSQ)+(RS|R)?"
+# ans = []
+# for i in finditer(pat, data):
+#     ans.append(len(i.group()))
+#
+# print(max(ans))
+
+with open("24_16388.txt") as f:
+    data = f.read()
+
+pat = r"(LMN|MN|N)?(KLMN)+(KLM|KL|K)?"
+
+ans = []
+for i in finditer(pat, data):
+    ans.append(len(i.group()))
+
+print(max(ans))
