b967486813cab667e6a1227030c6676b38c7f3a3
Роман <rmanuylov.r@gmail.com>
2025-05-20 16:53:04 +0200

    -13

--- Normeses/20-5-25/task 15 - 9162.py
+++ Normeses/20-5-25/task 15 - 9162.py
@@ -5,6 +5,6 @@ def f(x, y ,a):
     return (not p(x, y, a+13)) <= (p(28, y, 520) or p(x, 25, 800))
 
 for a in range(10000, -10000, -1):
-    if all(f(x, y, a) for x in range(100) for y in range(100)):
+    if all(f(x, y, a) for x in range(1, 100) for y in range(1, 100)):
         print(a)
         break
