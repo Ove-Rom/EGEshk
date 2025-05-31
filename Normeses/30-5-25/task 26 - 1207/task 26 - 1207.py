580adc37dbae26628825d5db8819fd418de1ec09
Роман <rmanuylov.r@gmail.com>
2025-05-31 12:58:07 +0200

    1611 90

---
+++ Normeses/30-5-25/task 26 - 1207/task 26 - 1207.py
@@ -0,0 +1,16 @@
+with open("26_1207.txt") as f:
+    s, n = map(int, f.readline().split())
+    data = sorted(int(i) for i in f)
+
+count = 0
+
+for c, n in zip(data, data[1:]):
+    if s - c - n >= 0:
+        s -= c
+        count += 1
+    elif s - n >= 0:
+        continue
+    elif s - n < 0:
+        break
+
+print(count + 1, c)
