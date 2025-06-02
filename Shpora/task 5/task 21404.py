8677471546b56c14954c4ecf8c9e891797534c25
Роман <rmanuylov.r@gmail.com>
2025-06-02 18:44:26 +0200

    176

---
+++ Shpora/task 5/task 21404.py
@@ -0,0 +1,12 @@
+for n in range(1, 1000):
+    r = f"{n:b}"
+    if r.count("1") % 2 == 0:
+        r += "0"
+        r = "10" + r[2:]
+    else:
+        r += "1"
+        r = "11" + r[2:]
+    r = int(r, 2)
+    if r > 480:
+        print(n)
+        break
