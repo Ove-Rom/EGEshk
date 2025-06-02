be64e802dc45a7c8b430f057e5595fe90f5520c3
Роман <rmanuylov.r@gmail.com>
2025-06-02 15:41:07 +0200

    8

--- Shpora/task 5/task 4585.py
+++ Shpora/task 5/task 4585.py
@@ -4,8 +4,8 @@ for n in range(1, 1000):
         r += "0"
         r = "10" + r[2:]
     else:
-        r += "0"
-        r = "10" + r[2:]
+        r += "1"
+        r = "11" + r[2:]
     r = int(r, 2)
     if r >= 16:
         print(n)
