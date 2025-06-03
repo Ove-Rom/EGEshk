2223117c7037ac02902a86687f48e87941e8c5a0
Роман <rmanuylov.r@gmail.com>
2025-06-03 14:25:55 +0200

    168

---
+++ Shpora/task 6/task 6 - 17625.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+lt(90)
+tracer(0)
+m = 10
+
+for i in range(10):
+    fd(22*m)
+    rt(90)
+    fd(16*m)
+    rt(90)
+
+pu()
+fd(m)
+rt(90)
+fd(m)
+lt(90)
+pd()
+
+for i in range(10):
+    fd(72*m)
+    rt(90)
+    fd(79)
+    rt(90)
+
+for x in range(-100*m, 100*m, m):
+    for y in range(-100*m, 100*m, m):
+        teleport(x, y)
+        dot(3, "crimson")
+
+done()
