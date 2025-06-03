12d310f6e02a74e9f6a64a34094173084264b6f1
Роман <rmanuylov.r@gmail.com>
2025-06-03 15:01:47 +0200

    83175

--- Shpora/task 8/task 8 - 17627.py
+++ Shpora/task 8/task 8 - 17627.py
@@ -4,6 +4,7 @@ count = 0
 
 for i in product("0123456789ABCDE", repeat=5):
     s = "".join(i)
+    if s[0] == "0": continue
     c1 = s.count("8") == 1
     c2 = sum(int(j, 15) > 9 for j in s) >= 2
     if c1 and c2:
