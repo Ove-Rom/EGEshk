b52ba6ba251f06d1f0aed30efe767d3cf60bfe30
Роман <rmanuylov.r@gmail.com>
2025-06-03 11:49:24 +0200

    66

---
+++ Shpora/task1/DZhk/task 4581.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = "ad de eg gc cf fa be bf ba".split()
+matrix = "37 367 125 56 34 247 126".split()
+
+print(*range(1, 8))
+
+for i in permutations("abcdefg"):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
