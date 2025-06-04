f00d62c44740207b5f5856f522963bb99bbfe935
Роман <rmanuylov.r@gmail.com>
2025-06-04 14:59:52 +0200

    24

---
+++ Shpora/task 13/task 13 - 14650.py
@@ -0,0 +1,16 @@
+from ipaddress import *
+
+from tqdm import tqdm
+
+ip1 = ip_address("216.54.187.235")
+ip2 = ip_address("216.54.174.128")
+ans = 0
+
+for mask in tqdm(range(10, 31)):
+    net1 = ip_network(f"{ip1}/{mask}", False)
+    net2 = ip_network(f"{ip2}/{mask}", False)
+    if ip1 in net1.hosts() and ip2 in net2.hosts():
+        if net1 != net2:
+            ans = mask
+
+print(ans)
