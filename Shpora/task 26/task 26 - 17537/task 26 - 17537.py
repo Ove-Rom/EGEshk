835b4267a48c109d81381ca01b7466ab20f0ef17
Роман <rmanuylov.r@gmail.com>
2025-06-06 16:43:52 +0200

    9991 5643

--- Shpora/task 26/task 26 - 17537/task 26 - 17537.py
+++ Shpora/task 26/task 26 - 17537/task 26 - 17537.py
@@ -8,9 +8,9 @@ maxRow = maxCol = 0
 for row, col in data:
     p[col] = min(p[col], row)
 
-for i in range(k-1):
-    if maxRow <= min(p[i]-1, p[i+1]-1):
-        maxRow = min(p[i]-1, p[i+1]-1)
-        maxCol = i+1
+for i in range(k - 1):
+    if maxRow <= min(p[i] - 1, p[i + 1] - 1):
+        maxRow = min(p[i] - 1, p[i + 1] - 1)
+        maxCol = i + 1
 
 print(maxRow, maxCol)
